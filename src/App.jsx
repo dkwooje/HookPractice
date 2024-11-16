a67fdf445548dbfafa975046f19edd888ec3ea3a
ohwooje <dkwooje@naver.com>
2024-11-16 15:39:16 +0900

    useEffect 기초 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 
-
-
-
 function App() {
 
   const [count, setCount] =useState(1);
@@ -16,10 +13,24 @@ function App() {
   function handleInput(e){
     setName(e.target.value)
   }
+/*
+  useEffect(function(){
+    console.log("랜더링");
+  },);  
 
   useEffect(function(){
-    console.log("렌더링");
-  })
+    console.log("count 변화");
+  }, [count]);  
+
+  useEffect(function(){
+    console.log("name 변화");
+  }, [name]);
+  */  
+
+  useEffect(function(){
+    console.log("마운팅");
+  }, []);
+
 
 return(
   <div>
