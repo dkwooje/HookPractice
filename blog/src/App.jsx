47d368bdae7e679489d59060198f5e9eaa6b8eb6
ohwooje <dkwooje@naver.com>
2024-11-16 14:25:49 +0900

    usestate1

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [time,setTime] = useState(1);
+
+  const handleClick = () =>{
+    setTime(time + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <span>현제 시작: {time}시</span>
+      <button onClick={handleClick}>Update</button>
     </div>
   );
 }
